9cd04e1b7debb3c4a1ffaeeecf445b2205d258cd
LoganneF <loganne.featherston@gmail.com>
2020-03-10 18:46:35 -0700

    dash partial working

--- controllers/shifts.js
+++ controllers/shifts.js
@@ -5,50 +5,31 @@ const Shift = require("../models/shifts.js");
 //___________________
 //7 Restful Routes
 //___________________
-// Index  : GET    '/swap'          
-// Show   : GET    '/swap/:id'     
-// New    : GET    '/swap/newShift'      
-// Create : POST   '/swap'          
-// Edit   : GET    '/swap/:id/edit'
-// Update : PUT    '/swap/:id'      
-// Delete : DELETE '/swap/:id'      
+// Index  : GET    '/'          
+// Show   : GET    '/:id'     
+// New    : GET    '/new'      
+// Create : POST   '/'          
+// Edit   : GET    '/:id/edit'
+// Update : PUT    '/:id'      
+// Delete : DELETE '/:id'      
 
 // NEW
-router.get("/newShift", (req, res) =>{
-    res.render("../shifts/new.ejs", {
+router.get("/new", (req, res) =>{
+    res.render("shifts/new.ejs", {
       currentUser: req.session.currentUser
     })
 })
 
-router.get('/seed', (req, res) => {
-  Shift.create(
-    [
-      {
-        name: "Ron",
-        date: 4/9/2020,
-        time: 9-5,
-        position: "Shift Supervisor"
-      }, 
-      {
-        name: "Veronica",
-        date: 4/1/2020,
-        time: 8-12,
-        position: "barista"
-      },
-    ]);
-    res.redirect("/swap");
-   
-});
 
 // DELETE
-router.delete("/swap/:id", (req, res) =>{
+router.delete("/:id", (req, res) =>{
   Shift.findByIdAndDelete(req.params.id, (err, data) =>{
-    res.redirect('/swap')
+    res.redirect('/')
   })
 })
 
 // EDIT
-router.get('/swap/:id/edit', (req,res) =>{
+router.get('/:id/edit', (req,res) =>{
   Shift.findById(req.params.id, (err, chosenShift) => {
     res.render(
       'edit.ejs',
@@ -62,21 +43,21 @@ router.get('/swap/:id/edit', (req,res) =>{
 
 
 // PUT
-router.put('/swap/:id', (req, res)=>{
+router.put('/:id', (req, res)=>{
   Shift.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err,updateModel) =>{
-    res.redirect('/swap')
+    res.redirect('/')
   })
 })
 
 // Create
 router.post("/", (req,res) =>{
     Shift.create(req.body, (err, data)=>{
-    res.redirect("/swap")
+    res.redirect("/")
 });
 });
 
 // Index
-router.get("/swap", (req,res) => {
+router.get("/", (req,res) => {
       Shift.find({}, (error, shift) => {
         res.render("index.ejs", {
           shift: shift,
@@ -86,7 +67,7 @@ router.get("/swap", (req,res) => {
 })
 
 //SHOW
-router.get('/swap/:id',(req,res)=>{
+router.get('/:id',(req,res)=>{
   Shift.findById(req.params.id, (err, foundShift) => { 
       res.render("show.ejs", {
         shift: foundShift,
